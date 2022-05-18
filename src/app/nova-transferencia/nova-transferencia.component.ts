505bc7186e62c0ce88f6af4c02c4bff79201dd6b
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2022-05-18 16:45:23 -0300

    Instalando, utilizando o servidor e um componente Angular!

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector: 'app-nova-transferencia',
+templateUrl: './nova-transferencia.component.html',
+styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class AppComponent {
+    title = 'bytebank';
+}
+
+export class NovaTransferenciaComponent{
+  title = 'bytebank';
+}
