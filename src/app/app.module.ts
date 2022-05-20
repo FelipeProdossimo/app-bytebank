f2577cddbadb73fb756d1cf115b0c6a87f7bb649
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2022-05-20 09:28:54 -0300

    App Completo - Ultimo módulo concluído Router
    Organizando rotas.
    
    - Não se esqueça de iniciar pelo Prompt de Comando do VS Code (npm start).
    
    - Abra sua pasta onde esta seus arquivo e abra com CMD. Siga abaixo
    
    C:/suaPasta/Exemplo
    
    Após entrar faça novamente pelo Prompt de Comando.
    (cd dados)
    
    Agora vem o comando de start.
    (json-server --watch db.json).
    
    Está pronto, bom uso de seu sistema.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AppRoutingComponent } from './app-routing-module';
 import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
@@ -9,12 +8,13 @@ import { ExtratoComponent } from './extrato/extrato.component';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
     {
